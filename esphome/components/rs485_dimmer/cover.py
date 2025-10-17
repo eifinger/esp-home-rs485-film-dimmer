09ebb2fb985bae905fe87174dc1f0ec879be6cfc
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2025-10-17 10:46:37 +0200

    Convert to cover

--- esphome/components/rs485_dimmer/cover.py
+++ esphome/components/rs485_dimmer/cover.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import light, uart
+from esphome.components import cover, uart
 from esphome.const import CONF_OUTPUT_ID
 from esphome import pins
 
@@ -9,12 +9,12 @@ DEPENDENCIES = ["uart"]
 
 rs485_dimmer_ns = cg.esphome_ns.namespace("rs485_dimmer")
 RS485Dimmer = rs485_dimmer_ns.class_(
-    "RS485Dimmer", light.LightOutput, cg.Component, uart.UARTDevice
+    "RS485Dimmer", cover.Cover, cg.Component, uart.UARTDevice
 )
 
 CONF_TX_ENABLE_PIN = "tx_enable_pin"
 
-CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend(
+CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
     {
         cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RS485Dimmer),
         cv.Required(CONF_TX_ENABLE_PIN): pins.gpio_output_pin_schema,
@@ -25,7 +25,7 @@ CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await cg.register_component(var, config)
-    await light.register_light(var, config)
+    await cover.register_cover(var, config)
     await uart.register_uart_device(var, config)
 
     tx_enable_pin = await cg.gpio_pin_expression(config[CONF_TX_ENABLE_PIN])
